8cb3e23ed3fb47402014804a71278a94aa4752b2
cam-shotter <cameronshotter@gmail.com>
2016-11-23 23:00:45 +1300

    added add one button

--- Tasklist.js
+++ Tasklist.js
@@ -36,9 +36,11 @@ class Tasklist extends Component {
           renderRow={this.renderRow.bind(this)}
         />
 
-      <TouchableHighlight>
-        
-      </TouchableHighlight>
+      <TouchableHighlight style={styles.button}>
+          <Text style={styles.buttonText}>
+            Add One
+          </Text>
+        </TouchableHighlight>
       </View>
     )
   }
@@ -54,6 +56,20 @@ const styles = StyleSheet.create({
     backgroundColor: '#F7F7F7',
     flex: 1,
     justifyContent: 'flex-start',
+  },
+  button: {
+    height: 60,
+    borderColor: '#05A5D1',
+    borderWidth: 2,
+    backgroundColor: '#333',
+    margin: 20,
+    justifyContent: 'center',
+    alignItems: 'center'
+  },
+  buttonText: {
+    color: '#FAFAFA',
+    fontSize: 20,
+    fontWeight: '600',
   }
 })
 
